9c2a4683edd1d9fa3a9c740db50d0a20c94b0ac4
LiangzeHao <hlzpangpang@gmail.com>
2018-11-01 14:32:12 -0700

    complete response home page and header nav

---
+++ scripts/header.js
@@ -0,0 +1,8 @@
+function displayMenu(){
+	if($("#menu_nav").css("display") == "none"){
+		$("#menu_nav").css("display","block");
+	}
+	else if($("#menu_nav").css("display") == "block"){
+		$("#menu_nav").css("display","none");
+	}
+}
